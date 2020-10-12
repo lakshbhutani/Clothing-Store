a76dd59c16da4992eea8a55a3edc0868103fb77e
laksh bhutani <laksh.bhutani@kelltontech.com>
2020-10-13 01:29:13 +0530

    SignIn-SignUpIn progress

---
+++ src/components/sign-in/sign-in.component.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import './sign-in.styles.scss';
+
+export default class SignIn extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            email: '',
+            password: '',
+        }
+    }
+
+    render() {
+        return (
+            <div className="sign-in">
+                <h2>I already have an account</h2>
+                <span>Sign in with your email and passwords</span>
+            </div>
+        )
+    }
+}
---
+++ src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import './sign-in-and-sign-up.styles.scss'
+
+const SignInAndSignUpPage = () => (
+    <div>
+        Sign In
+    </div>
+)
+export default SignInAndSignUpPage
