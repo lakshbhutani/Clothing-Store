702809c10b7018c53e917459cadc64797e95b000
laksh bhutani <laksh.bhutani@kelltontech.com>
2020-10-12 23:46:36 +0530

    aDding header component

---
+++ src/components/header/header.component.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Link } from 'react-router-dom' 
+// import 
+
+import './header.styles.scss';
+
+const Header = () => (
+    <div className="header">
+        <Link to='/'>Logo</Link>    
+        <div className="options">
+            <Link className="option" to='/shop'>SHOP</Link>
+            <Link className="option" to='/contact'>CONTACT</Link>
+        </div>
+    </div>
+)
+
+export default Header
