ec644dcac389fe269ae1f2e3232c99b17777fdb5
laksh bhutani <laksh.bhutani@kelltontech.com>
2020-10-28 21:20:26 +0530

    Css in js added

--- src/components/custom-button/custom-button.component.jsx
+++ src/components/custom-button/custom-button.component.jsx
@@ -1,10 +1,15 @@
 import React from "react";
+import { CustomButtonContainer } from './custom-button.styles'
 import "./custom-button.styles.scss";
 
 const CustomButton = ({ children, isGoogleSignIn, inverted, ...otherProps }) => {
   return (
+    // <CustomButtonContainer {...otherProps} >{children}</CustomButtonContainer>
+
     <button
-      className={`${inverted ? "inverted" : ''} custom-button`}
+      className={`${inverted ? 'inverted' : ''} ${
+        isGoogleSignIn ? 'google-sign-in' : ''
+      } custom-button`}
       {...otherProps}
     >
       {children}
